9533c10c665f731a2e8810615e673bf041209a31
nadhrahfuad <nadhrah.fuad@gmail.com>
2024-07-19 10:31:21 +0800

    .env

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,16 @@ export default defineConfig(({ mode }) => {
   return {
     define: {
       'process.env.REACT_APP_BASE_URL': JSON.stringify(env.REACT_APP_BASE_URL),
+
+      'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(env.REACT_APP_FIREBASE_API_KEY),
+
+      'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+
+      'process.env.REACT_APP_FIREBASE_STORAGE_BUCKET': JSON.stringify(env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+
+      'process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+
+      'process.env.REACT_APP_FIREBASE_APP_ID': JSON.stringify(env.REACT_APP_FIREBASE_APP_ID),
     },
     plugins: [react()],
   }
